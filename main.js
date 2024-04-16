78535d2416482708a3a68bda1be6142cb2ed4afe
majidali321 <mianmajid577@gmail.com>
2024-04-16 06:17:28 +0500

    atm machine

---
+++ main.js
@@ -0,0 +1,47 @@
+import inquirer from "inquirer";
+// initialize user balance pin and code
+let myBalance = 5000;
+let myPin = 4321;
+// welcome machine
+console.log("welcome to Majid ATM machine");
+let pinAnswer = await inquirer.prompt([
+    {
+        name: "pin",
+        type: "number",
+        message: "Plz ENter your Pin Code"
+    }
+]);
+if (pinAnswer.pin === myPin) {
+    console.log("loggin successfully");
+}
+else {
+    console.log("plz enter correct pin");
+}
+let operations = await inquirer.prompt([
+    {
+        name: "check",
+        type: "list",
+        message: "select an operation",
+        choices: ["withdraw amount", "check balance"]
+    }
+]);
+if (operations.check === "withdraw amount") {
+    let amountAns = await inquirer.prompt([
+        {
+            name: "amount",
+            type: "number",
+            message: "enter amoount to withdraw"
+        }
+    ]);
+    if (amountAns.amount > myBalance) {
+        console.log("Insufficient balance");
+    }
+    else {
+        myBalance -= amountAns.amount;
+        console.log(`${amountAns.amount} withdraw successfully`);
+        console.log(`your current balance is ${myBalance}`);
+    }
+}
+else if (operations.check === "check balance") {
+    console.log(`your balance is ${myBalance}`);
+}
